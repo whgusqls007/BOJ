d5826900b4d56a7a4da6a8d9faf5904a5d786fb4
unknown <jo7hb@naver.com>
2021-07-13 22:46:17 +0900

    1011_Fly me to the Alpha Centauri

--- 1011.cpp
+++ 1011.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 
+// typedef long long lld;
+
 using namespace std;
 
 int main() {
@@ -7,9 +9,21 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T, x, y;
+    int T;
+    long long x, y;
     cin >> T;
     for (int i = 0; i < T; i++) {
         cin >> x >> y;
+        long long dist     = y - x;
+        double squareRoot1 = sqrt(y - x);
+        int squareRoot2    = (int)squareRoot1;
+        if (squareRoot1 == squareRoot2) {
+            cout << squareRoot2 * 2 - 1 << "\n";
+        } else if (squareRoot2 * squareRoot2 < dist &&
+                   dist <= squareRoot2 * (squareRoot2 + 1)) {
+            cout << squareRoot2 * 2 << "\n";
+        } else {
+            cout << squareRoot2 * 2 + 1 << "\n";
+        }
     }
 }
